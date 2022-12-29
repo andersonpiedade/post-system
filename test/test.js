a914c03cea75f6b7d7eea9db3baf8e58f07ce364
Anderson Piedade <andrealfredo04@gmail.com>
2022-12-29 22:53:13 +0000

    testando conexão com sequelize antes de usar

---
+++ test/test.js
@@ -0,0 +1,13 @@
+// Testing sequelize before using 
+
+// const Sequelize = require('sequelize');
+// const sequelize = new Sequelize('noteapp', 'root', 'javascript_node', {
+// 	host: 'localhost',
+// 	dialect: 'mysql'
+// });
+
+// sequelize.authenticate().then(()=> {
+// 	console.log('Conectado com sucesso');
+// }).catch((error)=> {
+// 	console.error('ERRO:', error);
+// });
